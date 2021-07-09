fd66bc095ec7a9de7329835ae0eb6ef43f1df9b5
Kunishige-Yamato <tmc20247010@gmail.com>
2021-07-09 10:46:43 +0900

    Commit 21
    モード選択シーンの追加
    csファイルの整理
    モード選択のアニメーション制御

---
+++ Assets/CS/UI/backSelectMode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class backSelectMode : MonoBehaviour
+{
+    public GameObject menuCanvas;
+    public GameObject modeCanvas1;
+    public GameObject modeCanvas2;
+    public GameObject buttonCanvas;
+    public GameObject BG;
+    Animator animator,animator2,animator3;
+
+    void Start()
+    {
+        animator=menuCanvas.GetComponent<Animator>();
+        animator3=buttonCanvas.GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        BG.gameObject.GetComponent<Image>().color=new Color(0.471f,0.471f,0f,0.471f);
+        if(modeCanvas1.transform.position.x<400){
+            animator2=modeCanvas1.GetComponent<Animator>();
+        }
+        else{
+            animator2=modeCanvas2.GetComponent<Animator>();
+        }
+        animator.Play("MenuMove2");
+        animator2.Play("ModeMove2");
+        animator3.Play("ButtonMove2");
+    }
+}
---
+++ Assets/CS/UI/moveCanvas_1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moveCanvas_1 : MonoBehaviour
+{
+    public GameObject menuCanvas;
+    public GameObject modeCanvas1;
+    public GameObject modeCanvas2;
+    public GameObject buttonCanvas;
+    public GameObject BG;
+    Animator animator,animator2,animator3;
+
+    void Start()
+    {
+        animator=menuCanvas.GetComponent<Animator>();
+        if(gameObject.name=="StoryModeButton"){
+            animator2=modeCanvas1.GetComponent<Animator>();
+        }
+        if(gameObject.name=="EndlessModeButton"){
+            animator2=modeCanvas2.GetComponent<Animator>();
+        }
+        animator3=buttonCanvas.GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        if(gameObject.name=="StoryModeButton"){
+            BG.gameObject.GetComponent<Image>().color=new Color(0f,0.471f,0f,0.471f);
+        }
+        if(gameObject.name=="EndlessModeButton"){
+            BG.gameObject.GetComponent<Image>().color=new Color(0f,0f,0.471f,0.471f);
+        }
+        animator.Play("MenuMove1");
+        animator2.Play("ModeMove1");
+        animator3.Play("ButtonMove1");
+    }
+}
