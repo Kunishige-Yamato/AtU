4a8165916d32807cfd65b2ee2fd74dc3025d8844
Kunishige-Yamato <tmc20247010@gmail.com>
2021-10-05 15:52:42 +0900

    Commit 34
    シーン遷移ボタン機能追加
    タイトルconfig追加

---
+++ Assets/CS/UI/buttonConfToggle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonConfToggle : MonoBehaviour
+{
+    public GameObject ConfCanvas;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Onclick()
+    {
+        if(gameObject.name=="ConfigurationBtn")
+        {
+            ConfCanvas.SetActive(true);
+        }
+        else
+        {
+            ConfCanvas.SetActive(false);
+        }
+    }
+}
---
+++ Assets/CS/UI/buttonMoveScene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonMoveScene : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void Onclick()
+    {
+        switch(gameObject.name){
+            case "StartBtn":
+                SceneManager.LoadScene("selectMode");
+                break;
+            case "ScoreBtn":
+                Debug.Log("score move");
+                //SceneManager.LoadScene("score");
+                break;
+            case "AchievementBtn":
+                Debug.Log("achieve move");
+                //SceneManager.LoadScene("achive");
+                break;
+            case "BackTitleButton":
+                SceneManager.LoadScene("title");
+                break;
+        }
+    }
+}
