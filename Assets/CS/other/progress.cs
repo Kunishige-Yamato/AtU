92fdcbd8de141596f2bf658fe2978dc7e9e8ae27
Kunishige-Yamato <81519105+Kunishige-Yamato@users.noreply.github.com>
2022-01-27 10:08:32 +0900

    Commit kuni_47
    本番用にテスト用記述削除

--- Assets/CS/other/progress.cs
+++ Assets/CS/other/progress.cs
@@ -414,7 +414,6 @@ public class progress : MonoBehaviour
                     break;
             }
             //発表用テストデータ
-            csvFile = Resources.Load("CSV/test") as TextAsset;
             StringReader reader = new StringReader(csvFile.text);
 
             // , で分割しつつ一行ずつ読み込み，リストに追加していく
